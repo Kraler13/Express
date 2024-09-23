b32622092bfe498b2a71fd9d8f149aaa36860e10
fnowak <filip.nowak7@gmail.com>
2024-09-23 11:32:23 +0200

    część 5

---
+++ app/models/PostModel.ts
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+ 
+const Post = new mongoose.Schema(
+  {
+    title: String,
+    content: String,
+    author: String,
+  },
+  { timestamps: true }
+);
+ 
+module.exports = mongoose.model('Post', Post)
---
+++ index.ts
@@ -0,0 +1,48 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { title } from "process";
+
+	
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/express-blog");
+const Post = require('./app/models/PostModel')
+const hbs = require('express-handlebars');
+
+dotenv.config();
+
+const app = express();
+const port = 8080;
+app.engine("hbs", hbs.engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+
+type Example = {
+  id: string;
+}
+
+app.get("/mongoose/:id", function (req, res) {
+  Post.findById(req.params.id).then((post)=>{
+    res.render("home", {
+      title: post.title,
+      content: post.content,
+      displayTitle: true,
+      names: ["Adam", "Ola", "Kasia", "Tomek"],
+    });
+  }).catch((err)=>{
+    res.send(err)
+  })
+   
+});
+
+app.use('/files', express.static('public'))
+
+app.get('/', (req: Request<Example>, res: Response) => {
+  res.render("home", {title: 'my app',
+     content: 'lorem ipsum', 
+     showTitle: true,
+     names: ['Ola', 'Adam', 'Michał']});
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
